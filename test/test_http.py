08bf94107ed24bc2eca3b476126dd4b0f4c7aa20
Vollate <uint44t@gmail.com>
2023-11-30 01:49:35 +0800

    template(test): add test template

--- test/test_http.py
+++ test/test_http.py
@@ -1,7 +1,13 @@
-import http as def_http
-from src.protocal import http
+import requests
+from src.protocol import http
+
 
 def test_parser():
+    res = requests.get("http://example.com")
+    print(res)
+    #TODO
     pass
+
+
 def test_builder():
     pass
